5a3750175d03ba9d3f54dd9b5adbb361b9de8e42
Surendran Mahendran <surenspost@gmail.com>
2011-04-27 18:23:52 +0000

    Adding ANT build file for the project

---
+++ src/benchmarks/DiskIOBenchmark.java
@@ -0,0 +1,11 @@
+package benchmarks;
+
+import org.apache.commons.cli.*;
+
+public class DiskIOBenchmark {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world\n");
+	}
+
+}
