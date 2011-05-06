65559b34931b298bff2814d1c42b1f8befdddc6e
Surendran Mahendran <surenspost@gmail.com>
2011-05-06 09:40:24 +0000

    Adding a new base class for IO runnables

---
+++ src/benchmarks/WorkerThread.java
@@ -0,0 +1,11 @@
+package benchmarks;
+
+public abstract class WorkerThread implements Runnable{
+	public Thread threadInstance;
+	public abstract void run();
+	public abstract double getThroughput();
+	
+	public void setThread(Thread thread){
+		threadInstance = thread;
+	}
+}
