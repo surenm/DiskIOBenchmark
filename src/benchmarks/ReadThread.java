27373e95b28ec48b5e97329e8dc9d11c01643abc
Surendran Mahendran <surenspost@gmail.com>
2011-05-06 09:40:31 +0000

    Fixing ReadThread

--- src/benchmarks/ReadThread.java
+++ src/benchmarks/ReadThread.java
@@ -1,23 +1,37 @@
 package benchmarks;
 
 import java.io.*;
+import java.util.concurrent.CountDownLatch;
 
-public class ReadThread implements Runnable{
+public class ReadThread extends WorkerThread{
 	private String[] filesList;
 	private int chunkSize;
 	private double timeElapsed;
 	private byte[] str;
+	private long totalSize;
+	private double throughput ;
+	private CountDownLatch startLatch;
 	
-	ReadThread(String[] _fileList, int _chunkSize ){
+	ReadThread(String[] _fileList, int _chunkSize, CountDownLatch latch ){
 		filesList = _fileList;
 		chunkSize = _chunkSize;
 		timeElapsed = 0.0;
+		totalSize = 0;
+		for (String fileName : filesList) {
+			totalSize += Utils.get_file_size(fileName);
+		}
+		throughput = 0.0;
 		str = new byte[chunkSize];
+		startLatch = latch;
 	}
 	
-	public void run() {
+	public void run(){
+		try {
+			startLatch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		for (String fileName : filesList) {
-			
 			try {
 				// Start timer
 				long startTime = System.currentTimeMillis();
@@ -28,12 +42,17 @@ public class ReadThread implements Runnable{
 				
 				// End timer
 				long endTime = System.currentTimeMillis();
-				timeElapsed += ( endTime - startTime );
+				timeElapsed += (endTime - startTime)/1000;
 				
 			} catch (IOException e) {
 				e.printStackTrace();
 			} 
-			
 		}
+		
+		throughput = totalSize/timeElapsed;
+	}
+	
+	public double getThroughput(){
+		return Utils.round_off(throughput); 
 	}
 }
