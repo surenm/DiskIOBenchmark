3f0614ebd52714f85be70c251d0cc210c4ac6ece
Surendran Mahendran <surenspost@gmail.com>
2011-05-06 09:40:37 +0000

    Fixing WriteThread

--- src/benchmarks/WriteThread.java
+++ src/benchmarks/WriteThread.java
@@ -2,24 +2,34 @@ package benchmarks;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
 
-public class WriteThread implements	Runnable{
+public class WriteThread extends WorkerThread{
 	private String[] filesList;
 	private long blockSize;
 	private int chunkSize;
 	private double timeElapsed;
 	private byte[] str ;
+	private long totalSize;
+	private double throughput ;
+	private CountDownLatch startLatch;
 	
-	WriteThread(String[] _fileList, long _blockSize, int _chunkSize ){
+	WriteThread(String[] _fileList, long _blockSize, int _chunkSize, CountDownLatch latch){
 		filesList = _fileList;
 		blockSize = _blockSize;
 		chunkSize = _chunkSize;
 		timeElapsed = 0.0;
+		totalSize = blockSize * filesList.length / (1024*1024);
 		str = new byte[chunkSize];
-		Arrays.fill(str, (byte)'0');
+		startLatch = latch;
 	}
 	
 	public void run(){
+		try {
+			startLatch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		for (String fileName : filesList) {
 			long chunks = blockSize/chunkSize;
 			
@@ -29,16 +39,25 @@ public class WriteThread implements	Runnable{
 				
 				// Write @chunks chunks of @chunk_size data to the file
 				OutputStream unBufferedStream = new FileOutputStream(fileName);				
-				while(chunks-- != 0) unBufferedStream.write(str);
+				while(chunks-- != 0) {
+					unBufferedStream.write(str);
+				}
 				unBufferedStream.close();
 				
 				// End timer
 				long endTime = System.currentTimeMillis();
-				timeElapsed += ( endTime - startTime );
-				
+				timeElapsed += (endTime - startTime)/1000;
+					
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		}				
+			
+		}
+		
+		throughput = totalSize/(timeElapsed);
+	}
+	
+	public double getThroughput(){
+		return Utils.round_off(throughput); 
 	}
 }
